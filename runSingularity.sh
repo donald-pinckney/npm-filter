4b870c4651204033332c12b1aa3a8b9c8c084dc7
Donald Pinckney <donald_pinckney@icloud.com>
2023-07-21 10:24:11 -0400

    tweak script

--- runSingularity.sh
+++ runSingularity.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
-work_dir_host=`mktemp -d`
-echo "Using work dir: $work_dir_host"
-singularity run --compat --home $work_dir_host --bind configs/verbose.json:$work_dir_host/config.json:ro npm-filter.sif --repo_link https://github.com/expressjs/body-parser
+work_dir_host=$2
 
+# echo "Using work dir: $work_dir_host"
 
-#singularity shell --compat --home $work_dir_host --bind configs/verbose.json:$work_dir_host/config.json:ro npm-filter.sif --repo_link https://github.com/expressjs/body-parser
+if [ ! -f "$work_dir_host/config.json" ]; then
+    echo "$work_dir_host/config.json does not exist. This MUST be the config file to use for npm-filter."
+    exit 1
+fi
+
+if [ ! -d "$work_dir_host/the_package" ]; then
+    echo "$work_dir_host/the_package does not exist. This MUST be the input directory containing a JS package!"
+    exit 2
+fi
+
+singularity run --compat --home $work_dir_host npm-filter.sif --config config.json  --repo_local_dir the_package/
